cf490db521ac2e21a30388e5f7daded881c81ced
hro-maker <hrant.muradyan.1998@mail.ru>
2020-11-29 11:14:57 +0400

    react_step_1

---
+++ build/precache-manifest.ef9e71a3d3cdd1e5ca34c8a94ce69af1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "520d994eda8e486be27b5e3334801c70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d101af157b83291057d1",
+    "url": "/static/css/main.4a762ef7.chunk.css"
+  },
+  {
+    "revision": "6cc9540664a807f0a261",
+    "url": "/static/js/2.963f1a57.chunk.js"
+  },
+  {
+    "revision": "d101af157b83291057d1",
+    "url": "/static/js/main.2a9159bd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
