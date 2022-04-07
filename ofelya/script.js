7b05201aba4eacb27b9d2f2c4d07d4c9692f7ed5
Hrant Muradyan <hrant.muradyan.1998@mail.ru>
2022-04-07 16:42:19 +0400

    styles

--- ofelya/script.js
+++ ofelya/script.js
@@ -1,11 +1,16 @@
-console.log("hello");
-document.addEventListener("DOMContentLoaded", () => {
-  const aud = document.querySelector("audio");
-  aud.addEventListener("canplay", (a) => {
-    aud.play();
-  });
+$(document).ready(() => {
   const small = document.querySelector(".small");
   const arr = ["1.avif", "2.jpeg", "3.jpeg", "4.jpeg", "4.jpeg"];
+  // const aud = new Audio("./assets/music.mp3");
+  // console.log(aud);
+  // aud.audio.play();
+  var audio = document.createElement("AUDIO");
+  document.body.appendChild(audio);
+  audio.src = "./assets/musicc.mp3";
+  audio.loop = true;
+  document.addEventListener("click", () => {
+    audio.play();
+  });
   let counter = 0;
   setInterval(() => {
     if (counter >= 4) {
@@ -13,6 +18,11 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       counter++;
     }
-    small.style.background = `url(./assets/${arr[counter]}) center center/cover no-repeat`;
+    console.log(arr[counter]);
+    try {
+      small.style.background = `url(./assets/${arr[counter]}) center center/cover no-repeat`;
+    } catch (error) {
+      console.log(error);
+    }
   }, 5000);
 });
