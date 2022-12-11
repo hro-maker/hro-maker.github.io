303afcf922873e3483e810fef66f66accf996ca2
Hrant Muradyan <69071631+hro-maker@users.noreply.github.com>
2022-12-11 12:05:34 +0400

    Delete asyaaa directory

--- asyaaa/js/script.js
+++
@@ -1,9 +0,0 @@
-const btn =document.querySelector('.btn-open'),
-close =document.querySelector('.btn-close'),
-        decr=document.querySelector('.document_descr');
-
-        close.addEventListener('click',()=>{
-            decr.style.display="none"}) 
-    btn.addEventListener('click',()=>{
-        decr.style.display="block"})    
-    
